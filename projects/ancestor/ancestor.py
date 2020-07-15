a1f04b26ddf12e82a1e992bab71b146164936994
AuFeld <goldfeld.chase@gmail.com>
2020-07-15 19:15:00 -0400

    Day 2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -17,8 +17,6 @@ import sys
 sys.path.append('../graph')
 
 from util import Stack, Queue
-from graph import Graph
-
 
 def earliest_ancestor(ancestors, starting_node):
     # Lets create our dictionary to store parents for each child
