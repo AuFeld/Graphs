71facdfa167a7278bd219df036bde2302e11a81c
AuFeld <goldfeld.chase@gmail.com>
2020-07-15 19:09:53 -0400

    Day 2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,23 +1,67 @@
+# Understood
+
+# Plan
+## Graphs Problem Solving
+### Translate the problem
+#### Nodes = people
+#### Edges = when child has a parent
+
+### build our graph, or define our get_neighbors
+
+### choose algo
+#### either BFS or DFS
+
+''' BFS ''' 
+
 import sys
 sys.path.append('../graph')
 
-''' Find the earliest ancestor of a node '''
+from util import Stack, Queue
+from graph import Graph
 
 
 def earliest_ancestor(ancestors, starting_node):
-    # Create graph
-    graph = Graph()
+    # Lets create our dictionary to store parents for each child
+    parents = {}
+    # Now we're creating our empty queue and enqueue
+    # A path to our starting node
+    q = Queue()
+    q.enqueue([starting_node])
 
-    # Add vertices
-    for parent, child in ancestors:
-        graph.add_vertex(parent)
-        graph.add_vertex(child)
-    # Add edges, direction child > parent
-    for parent, child in ancestors:
-        graph.add_edge(child, parent)
+    for a in ancestors:
+        # key
+        k = a[1]
+        # value
+        v = a[0]
+        if k not in parents:
+            parents[k] = []
+        parents[k].append(v)
 
-    # if vert has no parent (eg, not empty set)
-    # return -1
+    # Second, will list to keep track of len from our longest path and node at that length
+    longest = [0, 0]
+    # While our queue is not empty:
+    while q.size() > 0:
+        # Dequeue our first path
+        path = q.dequeue()
+        # will now grab our last node from the path
+        node = path[-1]
+        # lets check if node has parents within our dictionary:
+        if node not in parents:
+          # if our, will now compare length of our current path to store longest.
+            # in addition, node at the distance
+            if len(path) > longest[0]:
+                longest = (len(path), node)
+            elif len(path) == longest and longest[1] > node:
+                longest = (len(path), node)
 
-if not graph.get_neighbors(starting_node):
-    return -1
+        else:
+            # if we have parents in the dict, will add a new path for each
+            # parent of our last_vertex:brea
+            for parent in parents[node]:
+                q.enqueue(path + [parent])
+    # if our starting node does not  have any parents, will return -1:
+    if longest[1] == starting_node:
+        return -1
+    else:
+        # if not, our earliest ancestor  will be should stored in longest
+        return longest[1]
