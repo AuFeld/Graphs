0be2a9dc2f190681eead39bc41cacd9a14b8ab95
AuFeld <goldfeld.chase@gmail.com>
2020-07-17 06:16:21 -0400

    Day 4

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -28,42 +28,6 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
-''' Begin traversal code '''
-
-graph = Graph()
-while len(graph.rooms) < len(room_graph):
-    this_room = player.current_room
-    if this_room.id not in graph.rooms:
-        graph.add_room(this_room)
-    # random pick from possible directions
-    possible_exits = this_room.get_exits()
-    direction = random.choice(possible_exits)
-    # pick a room not already in the graph
-    while this_room.get_room_in_direction(direction).id in graph.rooms and '?' not in graph.get_neighbors(this_room.get_room_in_direction(direction).id).values():
-        direction = random.choice(possible_exits)
-    # move in chosen direction
-    player.travel(direction)
-    new_room = player.current_room
-    # add this room if it isn't in graph
-    if new_room.id not in graph.rooms:
-        graph.add_room(new_room)
-    # and add it to the traversal path
-    traversal_path.append(direction)
-    # ensure this new room is connected with the old room
-    if new_room.id not in graph.get_neighbors(this_room.id).values():
-        graph.add_hall(this_room, new_room, direction)
-    # if no unexplored rooms, we're at and end
-    if '?' not in graph.get_neighbors(new_room.id).values() and graph.bfs(new_room.id) is not None:
-        # we populate the traversal path
-        for room_id in graph.bfs(new_room.id):
-            for cardinal in ['n', 's', 'e', 'w']:
-                room_dir = player.current_room.get_room_in_direction(
-                    cardinal)
-                if room_dir and room_dir.id == room_id:
-                    traversal_path.append(cardinal)
-                    player.travel(cardinal)
-
-''' End traversal code '''
 
 # TRAVERSAL TEST
 visited_rooms = set()
